c428c1202939f03ffe3cedac70a30ebacfbcb20e
Maria Luíza Ramos Teixeira <162145706+MariaLuizaTeixeira@users.noreply.github.com>
2025-07-04 17:50:39 -0300

    Create aula_01.py

---
+++ modulo_01/aula_01.py
@@ -0,0 +1,39 @@
+print('Olá, Mundo') 
+print(7 + 4)
+print('7' + '4')
+
+nome = 'Malu'
+idade = 16
+peso = 48
+print(nome, idade, peso)
+
+nome = input('Qual o seu nome?')
+idade = input('Qual a sua idade?')
+peso = input('Qual o seu peso?')
+print(nome, idade, peso)
+
+# EXERCICIO 1
+
+nome = input('Insira o sue nome: ')
+print("Seja bem vindo(a), " , nome)
+
+# EXERCICIO 2
+
+dia = input('Insira seu dia de nascimento: ')
+mes = input('Insira seu mês de nascimento:')
+ano = input('Insira seu ano de nascimento: ')
+print(nome, 'nasceu no dia ', dia,  ' d0 ', mes, ' de ', ano, '.')
+
+# EXERCICIO 3
+
+n1 = int(input('Insira o número 1: '))
+n2 = int(input('Insira o número 2: '))
+soma = n1 + n2
+print('A soma entre {} e {} vale {}' .format(n1, n2, soma))
+# print('A soma entre', n1, 'e', n2, 'vale', soma)
+
+
+
+
+
+
