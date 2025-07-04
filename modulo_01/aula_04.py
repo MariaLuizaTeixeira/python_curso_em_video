0ac15075a32da8cc097c02a3b12bd698afb17987
Maria Luíza Ramos Teixeira <162145706+MariaLuizaTeixeira@users.noreply.github.com>
2025-07-04 17:51:28 -0300

    Create aula_04.py

---
+++ modulo_01/aula_04.py
@@ -0,0 +1,34 @@
+import math
+import random
+numero = int(input('Digite um número: '))
+raiz = math.sqrt(numero)
+print('A raiz quadrada de {0} é {1}.'.format(numero, math.ceil(raiz)))
+
+numero = random.randint(1, 10)
+print(numero)
+
+# EXERCICIO 16
+
+numero = float(input('Insira um número: '))
+print('A porção inteira desse número é {0}'.format(math.trunc(numero)))
+
+# DESAFIO 17
+
+cateto_oposto = int(input('Insira o comprimento do cateto oposto:'))
+cateto_adjacente = int(input('Insira o comprimento do cateto adjacente:'))
+hipotenusa   = math.hypot(cateto_adjacente, cateto_oposto)
+print('Dado o valor dos catetos, o valor da hipotenusa é {0}'.format(hipotenusa))
+
+# DESAFIO 18
+
+angulo = int(input('Insira o valor do ângulo: '))
+print('Como o ângulo é de {0} graus, o seno é {1}, o cosseno é {2}, e a tangente é {3}'.format(angulo, math.sin(angulo), math.cos(angulo), math.tan(angulo)))
+
+# DESAFIO 19
+
+a1 = input('Insira o nome do aluno 1:')
+a2 = input('Insira o nome do aluno 2:')
+a3 = input('Insira o nome do aluno 3:')
+a4 = input('Insira o nome do aluno 4:')
+
+print('O aluno sorteado é {}.'.format(random.sample(a1, a2, a3, a4)))
