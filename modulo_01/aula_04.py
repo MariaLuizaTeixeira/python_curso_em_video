a67e9eb723e3c42480538f86ae86d99924ef038f
Maria Luíza Ramos Teixeira <162145706+MariaLuizaTeixeira@users.noreply.github.com>
2025-07-05 12:24:04 -0300

    Update aula_04.py

--- modulo_01/aula_04.py
+++ modulo_01/aula_04.py
@@ -30,5 +30,6 @@ a1 = input('Insira o nome do aluno 1:')
 a2 = input('Insira o nome do aluno 2:')
 a3 = input('Insira o nome do aluno 3:')
 a4 = input('Insira o nome do aluno 4:')
+lista = [a1, a2, a3, a4]
 
-print('O aluno sorteado é {}.'.format(random.sample(a1, a2, a3, a4)))
+print('O aluno sorteado é {}.'.format(random.choice(lista)))
