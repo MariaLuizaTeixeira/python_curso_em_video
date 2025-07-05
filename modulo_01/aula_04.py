23966e9ce9c16ba6358d5c1cea2875a24a169742
Maria Luíza Ramos Teixeira <162145706+MariaLuizaTeixeira@users.noreply.github.com>
2025-07-05 12:42:09 -0300

    Update aula_04.py

--- modulo_01/aula_04.py
+++ modulo_01/aula_04.py
@@ -33,3 +33,14 @@ a4 = input('Insira o nome do aluno 4:')
 lista = [a1, a2, a3, a4]
 
 print('O aluno sorteado é {}.'.format(random.choice(lista)))
+
+# DESAFIO 20
+
+a1 = str(input('Insira o nome do aluno 1: '))
+a2 = str(input('Insira o nome do aluno 2: '))
+a3 = str(input('Insira o nome do aluno 3: '))
+a4 = str(input('Insira o nome do aluno 4: '))
+lista = [a1, a2, a3, a4]
+random.shuffle(lista)
+print('A ordem de apresentação é: {0}.'.format(lista))
+
