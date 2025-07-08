708811a79a603dcb560e0da79ce9ce8c0a3bc91f
Maria Luíza Ramos Teixeira <162145706+MariaLuizaTeixeira@users.noreply.github.com>
2025-07-08 11:45:54 -0300

    Update aula_5.py

--- modulo_01/aula_5.py
+++ modulo_01/aula_5.py
@@ -1,1 +1,21 @@
+# FATIAMENTO
+
+frase = 'Malu Teixeira'
+print(frase[3:7])
+print(frase[3:7:2])
+print(frase[3:])
+print(frase[:3])
+print(frase.strip())
+print(frase.split())
+print('-'.join(frase))
+
+# EXERCICIO 22
+
+nome = str(input('Insira o seu nome: '))
+print('Analisando seu nome...')
+print('Seu nome em maiúsculas é {0}.'.format(nome.upper()))
+print('Seu nome em maiúsculas é {0}.'.format(nome.lower()))
+nome.replace(' ', '')
+print('Seu nome tem no total {0} letras.'.format(nome))
+print('Seu primeiro nome é {0} e tem {1} letras'.format(nome.split))
 
