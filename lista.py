f6155db95838decbe9f6f0c81ec2edf4332cea16
Maria Luíza Ramos Teixeira <162145706+MariaLuizaTeixeira@users.noreply.github.com>
2025-08-25 11:06:59 -0300

    Update lista.py

--- lista.py
+++ lista.py
@@ -1,21 +1,19 @@
-M = 1
-N = 1
-soma = 0
-while M != 0 and N != 0:
+numeros = []
+soma = cont = 0
+while True:
     M = int(input())
-    if M == 0:
-        break
+    numeros.append(M)
     N = int(input())
-    if N == 0:
+    if N == 0 or M == 0:
         break
-    if M > N:
-        maior = M
-        menor = N
-    else:
-        maior = N
-        menor = M
+    numeros.append(N)
+while cont + 1 < len(numeros):
+    if numeros[cont] > numeros [cont + 1]:
+        maior = numeros[cont]
+        menor = numeros[cont + 1]
     for i in range (menor, maior + 1):
         print(i, end=" ")
         soma+=i
     print(f'Sum={soma}')
     soma = 0
+    cont+=2
